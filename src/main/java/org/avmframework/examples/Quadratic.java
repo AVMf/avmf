91f96b3bb2e4108c68c39850af5c56d15e90a8f9
philmcminn <p.mcminn@sheffield.ac.uk>
2016-04-25 08:19:18 +0100

    Updated the pom to build with dependencies. Moved the examples into the outer examples pacakge

--- src/main/java/org/avmframework/examples/Quadratic.java
+++ src/main/java/org/avmframework/examples/Quadratic.java
@@ -1,49 +1,49 @@
-package org.avmframework.examples.allzeros;
+package org.avmframework.examples;
 
 import org.apache.commons.math3.random.MersenneTwister;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.avmframework.AVM;
 import org.avmframework.Monitor;
 import org.avmframework.TerminationPolicy;
-import org.avmframework.Vector;
 import org.avmframework.initialization.Initializer;
 import org.avmframework.initialization.RandomInitializer;
 import org.avmframework.localsearch.LocalSearch;
+import org.avmframework.Vector;
 import org.avmframework.objective.NumericObjectiveValue;
 import org.avmframework.objective.ObjectiveFunction;
 import org.avmframework.objective.ObjectiveValue;
-import org.avmframework.variable.IntegerVariable;
-import org.avmframework.variable.Variable;
+import org.avmframework.variable.FloatingPointVariable;
 
-public class AllZeros {
+public class Quadratic {
 
-    static final int NUM_VARS = 10;
-    static final int INIT = 0, MIN = -100000, MAX = 100000;
+    static final int A = 4, B = 10, C = 6;
+
+    static final int PRECISION = 1;
+    static final double INITIAL_VALUE = 0.0, MIN = -100000.0, MAX = 100000.0;
+
     static final int MAX_EVALUATIONS = 1000;
 
-    public static void main(String[] args) {
+    public static void main(java.lang.String[] args) {
 
         // define the objective function
         ObjectiveFunction objFun = new ObjectiveFunction() {
             @Override
             protected ObjectiveValue computeObjectiveValue(Vector vector) {
-                int distance = 0;
-                for (Variable var : vector.getVariables()) {
-                    distance += Math.abs(((IntegerVariable) var).getValue());
-                }
+                double x = ((FloatingPointVariable) vector.getVariable(0)).asDouble();
+                double y = (A * x * x) + (B * x) + C;
+                double distance = Math.abs(y);
                 return NumericObjectiveValue.LowerIsBetterObjectiveValue(distance, 0);
             }
         };
 
         // set up the vector to be optimized
         Vector vector = new Vector();
-        for (int i=0; i < NUM_VARS; i++) {
-            vector.addVariable(new IntegerVariable(INIT, MIN, MAX));
-        }
+        vector.addVariable(new FloatingPointVariable(INITIAL_VALUE, PRECISION, MIN, MAX));
 
-        // set up the local search ("IteratedPatternSearch" / "GeometricSearch" / "LatticeSearch")
-        String localSearchName = "IteratedPatternSearch";
-        if (args.length > 0) {
+        // set up the local search
+        // Geometric or lattice search can be used if "GeometricSearch" or "LatticeSearch" are provided as a parameter
+        java.lang.String localSearchName = "IteratedPatternSearch";
+        if (args.length > 0 && args[0].equals("GeometricSearch") || args[0].equals("LatticeSearch")) {
             localSearchName = args[0];
         }
         LocalSearch localSearch = LocalSearch.instantiate(localSearchName);
@@ -62,7 +62,7 @@ public class AllZeros {
         Monitor monitor = avm.search(vector, objFun);
 
         // output the results
-        System.out.println("Best solution: " + monitor.getBestVector());
+        System.out.println("Best solution: " + monitor.getBestVector().getVariable(0));
         System.out.println("Best objective value: " + monitor.getBestObjVal());
         System.out.println(
                 "Number of objective function evaluations: " + monitor.getNumEvaluations() +
