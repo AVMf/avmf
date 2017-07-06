725eb4e5a3df0d1e0c8cc17df2d18e2d6d6d79e5
Dipsy88 <Dipesh@Dipeshs-MacBook-Pro.local>
2017-07-06 16:21:58 +0200

    comments added

--- src/main/java/org/avmframework/examples/requirementassignment/RequirementObjectiveFunction.java
+++ src/main/java/org/avmframework/examples/requirementassignment/RequirementObjectiveFunction.java
@@ -15,23 +15,33 @@ import org.avmframework.variable.Variable;
 
 /* This example shows requirement assignment optimization problem.
  * The requirements need to be optimally assigned to the stakeholders
- * It involves three objectives: maximize the number of requirements  assigned to the stakeholders,
- * maximize familarity between each requirement and stakeholder, and 
- * minimize the workload between different stakeholders
+ * It involves three objectives: maximize the number of requirements  assigned to the stakeholders (ASSIGN),
+ * maximize familarity between each requirement and stakeholder (FAM), and 
+ * minimize the workload between different stakeholders (OWL)
  * Other objectives can be assigned as described in the paper
  * https://link.springer.com/article/10.1007/s10664-015-9418-0
  */
 
+/* Description the three objectives are provided below:
+ * ASSIGN calculates a value that tells how far we are from assigning all the requirements to the stakeholders.
+ * FAM is about computing the overall familiarity of stakeholders to assigned requirements
+ * OWL computes the overall difference of the workload between each pair of stakeholders
+ * We aim to maximize ASSIGN and FAM, and minimize OWL.
+ * Detailed description and formulae can be checked from
+ * https://link.springer.com/article/10.1007/s10664-015-9418-0
+ */
 
 public class RequirementObjectiveFunction extends ObjectiveFunction {
 	private List<Requirement> reqList = new ArrayList<Requirement>();
 	private RequirementOverview reqOverview = new RequirementOverview();
 	
 	// assign weights for the three objectives
+	// we assume that each objective has equal weight in this context
+	// the weights can be modified if some objective is more important than the others
 	final double WEIGHT_ASSIGNED_DIS= 0.333, WEIGHT_FAM_AVG = 0.333, WEIGHT_WL_DIFF = 0.333; 
 
-	 final int NUM_STAKEHOLDER = 10;		// number of stakeholder to distribute requirements
-	 final int MIN_FM=1, MAX_FM= 10;		// familarity
+	 final int NUM_STAKEHOLDER = 10;		// number of stakeholders to distribute requirements
+	 final int MIN_FM=1, MAX_FM= 10;		// familiarity of the requirements
 	 
 	 
     public RequirementObjectiveFunction(List<Requirement> reqList, RequirementOverview reqOverview) {
@@ -49,6 +59,7 @@ public class RequirementObjectiveFunction extends ObjectiveFunction {
 		double wl[] = new double[NUM_STAKEHOLDER];
 		int number[] = new int[NUM_STAKEHOLDER];
 
+		// initial setup
 		for (int i = 0; i < NUM_STAKEHOLDER; i++) {
 			fmst[i] = 0;
 			wl[i] = 0;
@@ -57,6 +68,8 @@ public class RequirementObjectiveFunction extends ObjectiveFunction {
 
     	for (int i=0; i<vector.getVariables().size();i++){
     		int value = Integer.parseInt(vector.getVariable(i).toString());
+    		//assign the requirement if the value is 0 or higher
+    		// -1 means the requirement is not assigned to any stakeholder
 	    	if (value>= 0){
 	    		assignedNumber++;
 	    		fmst[value]++;
@@ -91,7 +104,7 @@ public class RequirementObjectiveFunction extends ObjectiveFunction {
 		objFamAvg = numFormat(FamAvg);
 		objOWLDiffAvg = numFormat(OWLDiffAvg);
 		
-		// fitness need to be minimized
+		// fitness need to be minimized so we subtract 1 from objAssignedDis and objFamAvg
 		fitness = (1-objAssignedDis)* this.WEIGHT_ASSIGNED_DIS + (1-objFamAvg)* this.WEIGHT_FAM_AVG
 				+ (objOWLDiffAvg)* this.WEIGHT_WL_DIFF;
 		return NumericObjectiveValue.LowerIsBetterObjectiveValue(fitness, 0);
